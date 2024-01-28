157d4f7b320d5509754873d7701f11bf24b25a2d
phuonghd2001 <phuonghd2001@gmail.com>
2024-01-29 00:31:53 +0700

    Initial Commit

---
+++ NewMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewMovie
+//
+//  Created by macbook on 29/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
